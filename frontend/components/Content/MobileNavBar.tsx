cde16677df5c3f954ac697aebc3a1f8d86c0e713
zensayyy <zensayyy@gmail.com>
2023-03-20 16:08:34 +0100

    style mobile menu

--- frontend/components/Content/MobileNavBar.tsx
+++ frontend/components/Content/MobileNavBar.tsx
@@ -5,13 +5,13 @@ import { useState } from "react"
 
 const transLive = {
   out: { left: 0, top: 0, opacity: 0, },
-  in: { left: "-150%", top: "-4rem", opacity: 100 },
+  in: { left: "-45%", top: "-2.2rem", opacity: 100 },
   common: {},
   transitionProperty: "left, top, opacity"
 }
 const transPodcast = {
   out: { left: 0, top: 0, opacity: 0, },
-  in: { left: "-350%", top: 0, opacity: 100 },
+  in: { left: "-100%", top: "0rem", opacity: 100 },
   common: {},
   transitionProperty: "left, top, opacity"
 }
@@ -25,7 +25,7 @@ export default function MobileNavBar() {
           <UnstyledButton
             style={{ ...transition }}
             className="absolute text-white">
-            <Title className='hover:text-white'>Live</Title>
+            <Title order={3} className='hover:text-white'>Live</Title>
           </UnstyledButton>
         )}
       </Transition>
@@ -34,15 +34,15 @@ export default function MobileNavBar() {
           <UnstyledButton
             style={{ ...transition }}
             className="absolute -left-[350%] top-0 text-white">
-            <Title className='hover:text-white'>Podcasts</Title>
+            <Title order={3} className='hover:text-white'>Podcasts</Title>
           </UnstyledButton>
         )}
       </Transition>
       <UnstyledButton
-        className="p-2"
         onClick={() => setShowOptions(!showOptions)}>
-        <div className="text-white">
-          {showOptions ? <IconChevronRight size={42} /> : <IconChevronLeft size={42} />}
+        <div className="text-white flex items-center">
+          {showOptions ? <IconChevronRight size={36} /> : <IconChevronLeft size={36} />}
+          <Title order={2}>Zen.FM</Title>
         </div>
       </UnstyledButton>
     </div >
